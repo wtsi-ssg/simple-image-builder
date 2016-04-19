1eba7bfa46a2a759dc2a02224474d7d5825f1a22
Andy Perry <ap21@sanger.ac.uk>
2016-04-19 12:09:15 +0100

    Remove image-creation to change the branch [ci skip]

--- image-creation/ubuntu14.04-wordpress/pre-packer.sh
+++
@@ -1,4 +0,0 @@
-echo '#!/bin/bash' > scripts/install_wordpress.sh
-echo '' >> scripts/install_wordpress.sh
-cat scripts/web/enable_unattended_updates.sh scripts/web/make_english.sh scripts/web/setup_apache.sh scripts/web/setup_database.sh scripts/web/setup_postfix.sh scripts/web/setup_wordpress.sh >> scripts/install_wordpress.sh
-chmod 755 scripts/install_wordpress.sh
