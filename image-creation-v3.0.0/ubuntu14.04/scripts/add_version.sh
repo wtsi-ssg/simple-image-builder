c8691d6fe850cc3eea153d4fb1b1e67f581e8754
James Beal <James.Beal@sanger.ac.uk>
2016-07-18 14:23:17 +0100

    Remove old versions

--- image-creation-v3.0.0/ubuntu14.04/scripts/add_version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Script to create /etc/wtgc_version_id
-#
-
-
-VERSION_FILE=/etc/wtgc_version_id
-echo $image >${VERSION_FILE}
--- image-creation-v4.0.0/ubuntu14.04/scripts/web/enable_unattended_updates.sh
+++
@@ -1,9 +0,0 @@
-##----------------------------------------------------------------------
-## web/enable_unattended_updates.sh
-##----------------------------------------------------------------------
-
-## Enable automatic updates
-
-echo 'Unattended-Upgrade::Allowed-Origins {'        >  /etc/apt/apt.conf.d/51my-unattended-upgrades
-echo '  "${distro_id}:${distro_codename}-updates";' >> /etc/apt/apt.conf.d/51my-unattended-upgrades
-echo '];'                                           >> /etc/apt/apt.conf.d/51my-unattended-upgrades
