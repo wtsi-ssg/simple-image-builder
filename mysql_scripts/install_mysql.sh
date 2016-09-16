7348a060fb96bce7e143476f34dff06c47942310
Dave Holland <dh3@sanger.ac.uk>
2016-09-16 15:01:36 +0100

    first attempt at mariadb for CentOS

--- mysql_scripts/install_mysql.sh
+++ mysql_scripts/install_mysql.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-PACKAGES="mysql-server-5.6"
-
 ## This will be changed by cloud-init
-
 MYSQL_PASSWORD=supersecret
 
-
-export DEBIAN_FRONTEND=noninteractive
-
-if ( apt-get -y install ${PACKAGES} ) ; then
-	mysqladmin password ${MYSQL_PASSWORD}
+if [ "$USER" == "ubuntu" ] ; then
+    PACKAGES="mysql-server-5.6"
+    export DEBIAN_FRONTEND=noninteractive
+    if ( apt-get -y install ${PACKAGES} ) ; then
+            mysqladmin password ${MYSQL_PASSWORD}
+    fi
+fi
+if [ "$USER" == "centos" ] ; then
+    yum install -y mariadb-server mariadb
+    systemctl start mariadb.service
+    systemctl enable mariadb.service
+    mysqladmin password ${MYSQL_PASSWORD}
 fi
 
